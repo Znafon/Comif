c27c6ecdeeffc2f1d8d0a75172db5cf19e07330c
Corentin Couzinet <corentin.couzinet@gmail.com>
2016-03-26 19:50:24 +0100

    Backend: ajout Promotions

--- backend/start.sh
+++ backend/start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 python manage.py migrate
-exec gunicorn --bind 0.0.0.0:8000 --log-file=- comif.wsgi
+#exec gunicorn --bind 0.0.0.0:8000 --log-file=- comif.wsgi
+python manage.py runserver 0.0.0.0:8000
