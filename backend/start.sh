2c8e92af85407e3ba26c03e359b8392a598a88ed
Corentin COUZINET <corentin.couzinet@gmail.com>
2016-04-01 16:02:14 +0200

    Retour aux start.sh et docker-compose initiaux

--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 python manage.py migrate
-#exec gunicorn --bind 0.0.0.0:8000 --log-file=- comif.wsgi
-python manage.py runserver 0.0.0.0:8000
+exec gunicorn --bind 0.0.0.0:8000 --log-file=- comif.wsgi
